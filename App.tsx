9f5dac8ea730a0d475e24efdb83fae014b42e50a
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-06-25 13:07:44 +0530

    cleaned ios and andriod projects

--- App.tsx
+++ App.tsx
@@ -10,15 +10,25 @@
 
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
-// import Home from './src/screens/Home';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
 import OTP from './src/screens/OTP';
+import Login from './src/screens/Login';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NativeBaseProvider>
-      <OTP />
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="OTP" component={OTP} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 };
 export default App;
