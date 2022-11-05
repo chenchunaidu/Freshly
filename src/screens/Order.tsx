81285ae5de4afd0ee182b2af6bce68e9c89803e2
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-11-05 17:54:34 +0530

    fix: final commit

--- src/screens/Order.tsx
+++ src/screens/Order.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {VStack, Text} from 'native-base';
+import Geolocation from 'react-native-geolocation-service';
+import {Permission} from 'react-native';
 
 export default function Order() {
+  useEffect(() => {
+    if (PermissionStatus) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+        },
+        error => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  }, []);
   return (
     <VStack p="4">
       <Text>Order</Text>
